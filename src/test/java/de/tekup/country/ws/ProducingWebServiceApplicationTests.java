d468ca73dbddbba4c80d0db3b0670215c4194335
jemai-sameh <samehjemai98@gmail.com>
2023-04-28 11:06:02 +0100

    initialize project

---
+++ src/test/java/de/tekup/country/ws/ProducingWebServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package de.tekup.country.ws;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProducingWebServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
